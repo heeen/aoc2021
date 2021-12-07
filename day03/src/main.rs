05614e8a9a26f6d09d7eb8025a9d213921b2f36c
Florian Hänel <florian.haenel@lge.com>
2021-12-07 13:40:05 +0100

    day04

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,47 +1,82 @@
+use core::slice::Iter;
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("day03/input").expect("could not read input");
     let values: Vec<_> = contents
         .lines()
-        .map(|l| {
-            let parts = l.split_once(' ').unwrap();
-            (parts.0, parts.1.parse::<i32>().unwrap())
-        })
+        .map(|l| u64::from_str_radix(l, 2).unwrap())
         .collect();
 
-    let position = values
-        .iter()
-        .fold((0, 0), |accum: (i32, i32), value| match value.0 {
-            "forward" => (accum.0 + value.1, accum.1),
-            "up" => (accum.0, accum.1 - value.1),
-            "down" => (accum.0, accum.1 + value.1),
-            _ => panic!("unexpected movement"),
-        });
+    let width = contents.lines().last().unwrap().len();
+
+    let ones = count_bits(values.iter(), width);
+
+    let mut gamma: u32 = 0;
+    let mut epsilon: u32 = 0;
+    let mut bit = 1 << (width - 1);
+    for i in 0..width {
+        if ones[i] * 2 > values.len() {
+            gamma += bit;
+        } else {
+            epsilon += bit;
+        }
+        bit >>= 1;
+    }
+    println!("day 3 part 1: {} {} {}", gamma, epsilon, gamma * epsilon);
+
+    let mut oxygen = 0;
+    let mut filtered_o2 = values.clone();
+    let mut bit = 1 << (width - 1);
+    for i in 0..width {
+        let ones = count_bits(filtered_o2.iter(), width);
+
+        let more_ones = ones[i] * 2 >= filtered_o2.len();
+        filtered_o2 = filtered_o2
+            .into_iter()
+            .filter(|l| more_ones == (l & bit == bit))
+            .collect();
+        if filtered_o2.len() == 1 {
+            oxygen = filtered_o2[0];
+            break;
+        }
+        bit >>= 1;
+    }
+
+    let mut scrubber = 0;
+    let mut filtered_scrubber = values.clone();
+    let mut bit = 1 << (width - 1);
+    for i in 0..width {
+        let ones = count_bits(filtered_scrubber.iter(), width);
+        let more_ones = ones[i] * 2 >= filtered_scrubber.len();
+        filtered_scrubber = filtered_scrubber
+            .into_iter()
+            .filter(|l| more_ones != (l & bit == bit))
+            .collect();
+        if filtered_scrubber.len() == 1 {
+            scrubber = filtered_scrubber[0];
+            break;
+        }
+        bit >>= 1;
+    }
 
     println!(
-        "day 2 part 1: {} {} {}",
-        position.0,
-        position.1,
-        position.0 * position.1
+        "day 3 part 1: {} {} {}",
+        oxygen,
+        scrubber,
+        oxygen * scrubber
     );
+}
 
-    let position = values
-        .iter()
-        .fold((0, 0, 0), |accum, value| {
-            let (pos, depth, aim) = accum;
-            let (command, value) = *value;
-            match command {
-            "forward" => (pos + value, depth + aim * value, aim),
-            "up" => (pos, depth, aim - value),
-            "down" => (pos, depth, aim + value),
-            _ => panic!("unexpected movement"),
-        }});
-
-    println!(
-        "day 2 part 2: {} {} {}",
-        position.0,
-        position.1,
-        position.0 * position.1
-    );
+fn count_bits(values: Iter<u64>, width: usize) -> Vec<usize> {
+    values.fold(vec![0usize; width], |mut accum, l| {
+        let mut bit = 1 << (width - 1);
+        for i in 0..width {
+            if l & bit == bit {
+                accum[i] += 1;
+            }
+            bit >>= 1;
+        }
+        accum
+    })
 }
---
+++ day04/src/main.rs
@@ -0,0 +1,90 @@
+use std::fs;
+
+#[derive(Copy, Clone, Debug)]
+struct Field {
+    pub value: i32,
+    pub marked: bool,
+}
+#[derive(Debug)]
+struct Bingoboard {
+    pub fields: [[Field; 5]; 5],
+    pub row_marked: [usize; 5],
+    pub col_marked: [usize; 5],
+    pub won: bool,
+}
+
+fn main() {
+    let contents = fs::read_to_string("day04/input").expect("could not read input");
+    let mut lines = contents.lines();
+    let moves: Vec<_> = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|e| e.parse::<i32>().unwrap())
+        .collect();
+
+    let mut boards = Vec::new();
+    while lines.next().is_some() {
+        let mut board = Bingoboard {
+            fields: [[Field {
+                value: 0,
+                marked: false,
+            }; 5]; 5],
+            row_marked: [0; 5],
+            col_marked: [0; 5],
+            won: false,
+        };
+        for y in 0..5 {
+            let line = lines.next();
+            let row = line.unwrap().split_ascii_whitespace().enumerate();
+            for entry in row {
+                board.fields[y][entry.0].value = entry.1.parse().unwrap()
+            }
+        }
+        println!("board: {:?}", board);
+        boards.push(board);
+    }
+
+    let mut won_count = 0;
+    for draw in moves {
+        println!("drawn: {}", draw);
+        for (bi, board) in boards.iter_mut().enumerate() {
+            if board.won {
+                continue;
+            }
+            for y in 0..5 {
+                for x in 0..5 {
+                    if board.fields[y][x].value == draw {
+                        //println!("board {} hit {} {}", bi, x, y);
+                        board.fields[y][x].marked = true;
+                        board.col_marked[x] += 1;
+                        board.row_marked[y] += 1;
+                        if board.col_marked[x] == 5 || board.row_marked[y] == 5 {
+                            let score = board.get_score(draw);
+                            println!("BINGO board {}: score {}", bi, score);
+                            board.won = true;
+                            won_count += 1;
+                        }
+                    }
+                }
+            }
+        }
+        if won_count == boards.len() {
+            break;
+        }
+    }
+}
+impl Bingoboard {
+    pub(crate) fn get_score(&self, last: i32) -> i32 {
+        let mut score = 0;
+        /*    The score of the winning board can now be calculated. Start by finding the sum of all unmarked numbers on that board; in this case, the sum is 188. Then, multiply that sum by the number that was just called when the board won, 24, to get the final score, 188 * 24 = 4512.*/
+        for y in 0..5 {
+            for x in 0..5 {
+                if self.fields[y][x].marked == false {
+                    score += self.fields[y][x].value
+                }
+            }
+        }
+        return score * last;
+    }
+}
