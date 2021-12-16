abd4ca64b5967c746ec8e5a33c9e0b9844863b33
Florian Hänel <florian.haenel@lge.com>
2021-12-16 13:33:22 +0100

    day10 p2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,19 +2,20 @@ use core::panic;
 use std::{collections::HashMap, fs};
 
 trait Parser {
-    fn parse(&mut self, line: Vec<char>) -> Option<char>;
+    fn parse_braces(&self) -> Result<char, Vec<char>>;
 }
 
-impl Parser for Vec<char> {
-    fn parse(&mut self, line: Vec<char>) -> Option<char> {
-//        println!("parsing: {:?}", line);
-        for c in line {
+impl Parser for str {
+    fn parse_braces(&self) -> Result<char, Vec<char>> {
+        let mut stack: Vec<char> = Vec::new();
+        println!("parsing: {:?}", self);
+        for c in self.chars() {
             match c {
                 '(' | '[' | '{' | '<' => {
-                    self.push(c);
+                    stack.push(c);
                 }
                 _ => {
-                    if let Some(opened) = self.pop() {
+                    if let Some(opened) = stack.pop() {
                         let expected = match opened {
                             '(' => ')',
                             '[' => ']',
@@ -24,35 +25,55 @@ impl Parser for Vec<char> {
                         };
                         if c != expected {
                             println!("Expected {}, found {}", expected, c);
-                            return Some(c);
+                            return Ok(c);
                         }
                     }
                 }
             }
         }
-        println!("remaining stack {:?}", self);
-        None
+        println!("remaining stack {:?}", stack);
+        Err(stack)
     }
 }
 
 fn main() {
-    let contents = fs::read_to_string("day10/input_simple").expect("could not read input");
-    let input = contents.lines().map(|l| l.chars().collect());
-
+    let contents = fs::read_to_string("day10/input").expect("could not read input");
+    let input = contents.lines();
+    let mut completion_scores = Vec::new();
     let mut scores: HashMap<char, i32> = HashMap::new();
     for line in input {
-        let mut stack: Vec<char> = Vec::new();
-        if let Some(illegal) = stack.parse(line) {
-            let e = scores.entry(illegal).or_default();
-            *e += 1;
+        match line.parse_braces() {
+            Ok(illegal) => {
+                let e = scores.entry(illegal).or_default();
+                *e += 1;
+            }
+            Err(stack) => {
+                let completion_score = stack.iter().rev().fold(0u64, |a, c| {
+                    a * 5
+                        + match c {
+                            '(' => 1,
+                            '[' => 2,
+                            '{' => 3,
+                            '<' => 4,
+                            _ => panic!("unexpected stack value {}", c),
+                        }
+                });
+                println!("completion score {}", completion_score);
+                completion_scores.push(completion_score);
+            }
         }
     }
-    let score = scores.iter().fold(0, |a, e| a + e.1 * match e.0 {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        _ => 0,
+    let score = scores.iter().fold(0, |a, e| {
+        a + e.1
+            * match e.0 {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                _ => 0,
+            }
     });
-    println!("scores: {:?} {}", scores, score);
+
+    completion_scores.sort();
+    println!("scores: {:?} {} completion score {:?}", scores, score, completion_scores[completion_scores.len()/2]);
 }
