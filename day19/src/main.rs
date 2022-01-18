5c6fdc738cb330107a4dd0e118d59097a6b8d0d6
Florian Hänel <florian.haenel@lge.com>
2022-01-18 13:56:45 +0100

    day19wip

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,224 @@
+use enum_iterator::IntoEnumIterator;
+use itertools::Itertools;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct Pos {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl std::ops::Add for Pos {
+    type Output = Pos;
+    fn add(self, rhs: Self) -> Self::Output {
+        Pos {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+impl std::ops::Sub for Pos {
+    type Output = Pos;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Pos {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+#[derive(Debug, IntoEnumIterator, PartialEq, Clone, Copy)]
+enum UpAxis {
+    X,
+    NegativeX,
+    Y,
+    NegativeY,
+    Z,
+    NegativeZ,
+}
+
+#[derive(Debug, IntoEnumIterator, PartialEq, Clone, Copy)]
+enum Rotation {
+    Deg0,
+    Deg90,
+    Deg180,
+    Deg279,
+}
+
+impl Pos {
+    fn transformed(&self, orientation: (UpAxis, Rotation)) -> Self {
+        let (x, y, z) = (self.x, self.y, self.z);
+        let (x, y, z) = match orientation.0 {
+            UpAxis::X => (x, y, z),
+            UpAxis::NegativeX => (-x, y, -z),
+            UpAxis::Y => (y, -x, z),
+            UpAxis::NegativeY => (-y, x, z),
+            UpAxis::Z => (z, y, -x),
+            UpAxis::NegativeZ => (-z, y, x),
+        };
+        let (x, y, z) = match orientation.1 {
+            Rotation::Deg0 => (x, y, z),
+            Rotation::Deg90 => (x, z, -y),
+            Rotation::Deg180 => (x, -y, -z),
+            Rotation::Deg279 => (x, -z, y),
+        };
+        Pos { x, y, z }
+    }
+}
+
+#[test]
+fn test_transform() {
+    let mut results = HashSet::new();
+    for orientation in UpAxis::into_enum_iter().cartesian_product(Rotation::into_enum_iter()) {
+        let p = Pos { x: 1, y: 2, z: 3 }.transformed(orientation);
+        assert!(!results.contains(&p));
+        results.insert(p);
+    }
+}
+
+#[derive(Debug)]
+struct Scanner {
+    beacons: Vec<Pos>,
+    octants: [Vec<usize>; 8],
+    constellations: Vec<Constellation>,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Constellation {
+    distance: i32,
+    indices: (usize, usize),
+    scanner: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut scanners =
+        fs::read_to_string("day19/input")
+            .unwrap()
+            .lines()
+            .fold(Vec::new(), |mut a, line| {
+                if line.starts_with("---") {
+                    a.push(Scanner::new());
+                } else if !line.is_empty() {
+                    let mut parts = line.split(',').map(|p| p.parse::<i32>().unwrap());
+                    let last = a.last_mut().unwrap();
+                    let (x, y, z) = (
+                        parts.next().unwrap(),
+                        parts.next().unwrap(),
+                        parts.next().unwrap(),
+                    );
+
+                    let octant = if x.signum() > 0 { 1 } else { 0 }
+                        + if y.signum() > 0 { 2 } else { 0 }
+                        + if z.signum() > 0 { 4 } else { 0 };
+                    last.octants[octant].push(last.beacons.len());
+                    last.beacons.push(Pos { x, y, z });
+                }
+                a
+            });
+
+    let mut constellations_scanners = HashMap::new();
+
+    for (scanner_index, scanner) in scanners.iter_mut().enumerate() {
+        println!("--- scanner {scanner_index} ---");
+        for (octant, beacons) in scanner.octants.iter().enumerate() {
+            //            println!("octant {octant} {beacons:?}");
+            let min_dist = beacons
+                .iter()
+                .tuple_combinations()
+                .map(|(c_a, c_b)| {
+                    let b_a = scanner.beacons[*c_a];
+                    let b_b = scanner.beacons[*c_b];
+                    let d = (b_a.x - b_b.x, b_a.y - b_b.y, b_a.z - b_b.z);
+                    let dist_sq = d.0 * d.0 + d.1 * d.1 + d.2 * d.2;
+                    (dist_sq, *c_a, *c_b)
+                })
+                .min_by_key(|e| e.0)
+                .unwrap();
+
+            let constellation = Constellation {
+                distance: min_dist.0,
+                indices: (min_dist.1, min_dist.2),
+                scanner: scanner_index,
+            };
+            scanner.constellations.push(constellation);
+            constellations_scanners
+                .entry(constellation.distance)
+                .or_insert_with(Vec::new)
+                .push(constellation);
+        }
+    }
+    for (distance, constellations) in constellations_scanners {
+        if constellations.len() < 2 {
+            continue;
+        }
+        println!("constellation {distance:?} scanner constellations: {constellations:?}");
+        for (comb_a, comb_b) in constellations.iter().tuple_combinations() {
+            check_match(&scanners, comb_a, comb_b);
+        }
+    }
+}
+
+fn check_match(
+    scanners: &[Scanner],
+    c_a: &Constellation,
+    c_b: &Constellation,
+) -> Option<((UpAxis, Rotation), Pos, HashSet<Pos>)> {
+    let (scanner_a, scanner_b) = (&scanners[c_a.scanner], &scanners[c_b.scanner]);
+    let (a_0, a_1) = (
+        scanner_a.beacons[c_a.indices.0],
+        scanner_a.beacons[c_a.indices.1],
+    );
+    let (b_0, b_1) = (
+        scanner_b.beacons[c_b.indices.0],
+        scanner_b.beacons[c_b.indices.1],
+    );
+
+    let v_a = a_1 - a_0;
+    let v_a_inv = a_0 - a_1;
+    let v_b = b_1 - b_0;
+    println!("matching {c_a:?} with {c_b:?}\n vectors {v_a:?} {v_b:?}");
+
+    let mut combined_beacons = HashSet::new();
+    for beacon in scanner_a.beacons.iter() {
+        combined_beacons.insert(*beacon);
+    }
+
+    for orientation in UpAxis::into_enum_iter().cartesian_product(Rotation::into_enum_iter()) {
+        let transformed = v_b.transformed(orientation);
+        let dist = match transformed {
+            v_a => Some(a_0 - b_0.transformed(orientation)),
+            v_a_inv => Some(a_0 - b_1.transformed(orientation)),
+            _ => None,
+        };
+
+        if let Some(dist) = dist {
+            for beacon in scanner_b.beacons.iter() {
+                let p = beacon.transformed(orientation) + dist;
+                if combined_beacons.contains(&p) {
+                    println!("both a and b: {beacon:?} -> {p:?}");
+                } else {
+                    println!("only in b: {beacon:?} {p:?}");
+                    combined_beacons.insert(p);
+                }
+            }
+            return Some((orientation, dist, combined_beacons));
+        }
+    }
+
+    None
+}
+impl Scanner {
+    fn new() -> Self {
+        Scanner {
+            beacons: Vec::new(),
+            octants: [(); 8].map(|_| Vec::new()),
+            constellations: Vec::new(),
+        }
+    }
 }
