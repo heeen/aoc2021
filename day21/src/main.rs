a1ac68484fcc040b58b6c65c53dc2d92395c715c
Florian Hänel <florian.haenel@lge.com>
2022-01-22 21:00:08 +0100

    wip day21

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -20,12 +20,13 @@ impl Die for DeterministicDie {
     }
 }
 fn main() {
-/*
-    let mut player1_pos = 4u32 - 1;
-    let mut player2_pos = 8u32 - 1;
-*/
+    let simple = false;
     let mut player1_pos = 1u32 - 1;
     let mut player2_pos = 3u32 - 1;
+    if simple {
+        player1_pos = 4u32 - 1;
+        player2_pos = 8u32 - 1;
+    }
     let mut player1_score = 0u32;
     let mut player2_score = 0u32;
     let mut die = DeterministicDie::new();
@@ -58,3 +59,16 @@ impl DeterministicDie {
         DeterministicDie { roll_count: 0 }
     }
 }
+
+/*
+sum combinations
+3 	1
+4 	3
+5 	6
+6 	7
+7 	6
+8 	3
+9 	1
+-----
+    27
+*/
