dcfbab5b47e9e758db98edfea2c4cbbacefaec90
Florian Hänel <florian.haenel@lge.com>
2022-01-31 13:42:34 +0100

    wip

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use itertools::Itertools;
 struct DeterministicDie {
     roll_count: u32,
@@ -19,14 +21,14 @@ impl Die for DeterministicDie {
         self.roll_count
     }
 }
-fn main() {
-    let simple = false;
-    let mut player1_pos = 1u32 - 1;
-    let mut player2_pos = 3u32 - 1;
-    if simple {
-        player1_pos = 4u32 - 1;
-        player2_pos = 8u32 - 1;
+
+impl DeterministicDie {
+    pub(crate) fn new() -> Self {
+        DeterministicDie { roll_count: 0 }
     }
+}
+
+fn deterministic_game(mut player1_pos: u32, mut player2_pos: u32) {
     let mut player1_score = 0u32;
     let mut player2_score = 0u32;
     let mut die = DeterministicDie::new();
@@ -54,10 +56,77 @@ fn main() {
         die.roll_count() * player1_score.min(player2_score)
     );
 }
-impl DeterministicDie {
-    pub(crate) fn new() -> Self {
-        DeterministicDie { roll_count: 0 }
+
+fn main() {
+    let simple = false;
+    let mut player1_pos = 1u32 - 1;
+    let mut player2_pos = 3u32 - 1;
+    if simple {
+        player1_pos = 4u32 - 1;
+        player2_pos = 8u32 - 1;
     }
+    deterministic_game(player1_pos, player2_pos);
+    quantum_game(player1_pos, player2_pos);
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PlayerState {
+    pos: u32,
+    score: u32,
+    choices: u64,
+}
+
+fn quantum_rolls(state: &PlayerState, out: &mut Vec<PlayerState>) {
+    out.append(
+        &mut [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+            .iter()
+            .map(|(offset, choices)| {
+                let pos = (state.pos + offset) % 10;
+                PlayerState {
+                    pos,
+                    choices: state.choices * choices,
+                    score: state.score + pos + 1,
+                }
+            })
+            .collect(),
+    );
+}
+
+fn win_table(start: u32) -> HashMap<i32, Vec<PlayerState>> {
+    let mut table = HashMap::new();
+    let mut workqueue = vec![PlayerState {
+        pos: start,
+        score: 0,
+        choices: 1,
+    }];
+    let mut round = 0;
+    while !workqueue.is_empty() {
+        println!("--- round {round} ---");
+        let (wins, cont) = workqueue
+            .iter()
+            .fold(Vec::new(), |mut a, s| {
+                quantum_rolls(s, &mut a);
+                a
+            })
+            .iter()
+            .partition(|e| e.score >= 21);
+        workqueue = cont;
+
+        for s in &wins {
+            println!("win pos {} score {} choices {}", s.pos, s.score, s.choices)
+        }
+        table.insert(round, wins);
+        for s in &workqueue {
+            println!("pos {} score {} choices {}", s.pos, s.score, s.choices)
+        }
+        round += 1;
+    }
+   table
+}
+fn quantum_game(player1_pos: u32, player2_pos: u32) {
+    let p1_wins = win_table(player1_pos);
+    let p2_wins = win_table(player2_pos);
+
 }
 
 /*
