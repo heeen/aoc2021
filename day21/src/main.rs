211848a6242d24f08de47c0c7da81233302914b9
Florian Hänel <florian.haenel@lge.com>
2022-01-31 13:42:34 +0100

    day21

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use itertools::Itertools;
 struct DeterministicDie {
     roll_count: u32,
@@ -19,14 +21,14 @@ impl Die for DeterministicDie {
         self.roll_count
     }
 }
-fn main() {
-    let simple = false;
-    let mut player1_pos = 1u32 - 1;
-    let mut player2_pos = 3u32 - 1;
-    if simple {
-        player1_pos = 4u32 - 1;
-        player2_pos = 8u32 - 1;
+
+impl DeterministicDie {
+    pub(crate) fn new() -> Self {
+        DeterministicDie { roll_count: 0 }
     }
+}
+
+fn deterministic_game(mut player1_pos: u32, mut player2_pos: u32) {
     let mut player1_score = 0u32;
     let mut player2_score = 0u32;
     let mut die = DeterministicDie::new();
@@ -54,10 +56,29 @@ fn main() {
         die.roll_count() * player1_score.min(player2_score)
     );
 }
-impl DeterministicDie {
-    pub(crate) fn new() -> Self {
-        DeterministicDie { roll_count: 0 }
+
+fn main() {
+    let simple = false;
+    let mut player1_pos = 1u32 - 1;
+    let mut player2_pos = 3u32 - 1;
+    if simple {
+        player1_pos = 4u32 - 1;
+        player2_pos = 8u32 - 1;
     }
+    deterministic_game(player1_pos, player2_pos);
+    let (p1_wins, p2_wins) = quantum_game(player1_pos, player2_pos);
+    if simple {
+        assert_eq!(p1_wins, 444356092776315);
+        assert_eq!(p2_wins, 341960390180808);
+    }
+    println!("more wins: {}", p1_wins.max(p2_wins));
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PlayerState {
+    pos: u32,
+    score: u32,
+    choices: u64,
 }
 
 /*
@@ -72,3 +93,87 @@ sum combinations
 -----
     27
 */
+fn quantum_rolls(state: &PlayerState, out: &mut Vec<PlayerState>) {
+    out.append(
+        &mut [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+            .iter()
+            .map(|(offset, choices)| {
+                let pos = (state.pos + offset) % 10;
+                PlayerState {
+                    pos,
+                    choices: state.choices * choices,
+                    score: state.score + pos + 1,
+                }
+            })
+            .collect(),
+    );
+}
+
+fn win_table(start: u32, first: bool) -> HashMap<i32, (Vec<PlayerState>, Vec<PlayerState>)> {
+    let mut table = HashMap::new();
+    let mut workqueue = vec![PlayerState {
+        pos: start,
+        score: 0,
+        choices: 1,
+    }];
+    let mut round = 0;
+    while !workqueue.is_empty() {
+//        println!("--- round {round} ---");
+        let (wins, mut cont): (Vec<PlayerState>, Vec<PlayerState>) = workqueue
+            .iter()
+            .fold(Vec::new(), |mut a, s| {
+                quantum_rolls(s, &mut a);
+                a
+            })
+            .iter()
+            .partition(|e| e.score >= 21);
+
+/*
+        for s in &wins {
+            println!("win pos {} score {} choices {}", s.pos, s.score, s.choices)
+        }
+*/
+        table.insert(round, (wins, cont.clone()));
+
+/*        for s in &mut cont {
+            println!("pos {} score {} choices {}", s.pos, s.score, s.choices);
+        }
+*/
+        workqueue = cont;
+        round += 1;
+    }
+    table
+}
+fn quantum_game(player1_pos: u32, player2_pos: u32) -> (u64, u64) {
+    let p1_results = win_table(player1_pos, true);
+    let p2_results = win_table(player2_pos, false);
+    let mut p1_win_sum = 0;
+    let mut p2_win_sum = 0;
+
+    let mut p1_loss_choices = 1;
+    let mut p2_loss_choices = 1;
+    for i in 0..p1_results.len() as i32 {
+        let (p1_wins, p1_losses) = &p1_results[&i];
+        let (p2_wins, p2_losses) = &p2_results[&i];
+        let p1_win_choices = p1_wins.iter().map(|r| r.choices).sum::<u64>();
+
+        p1_win_sum += p2_loss_choices * p1_win_choices;
+        p1_loss_choices = p1_losses.iter().map(|r| r.choices).sum::<u64>();
+        let p2_win_choices = p2_wins.iter().map(|r| r.choices).sum::<u64>();
+        p2_win_sum += p1_loss_choices * p2_win_choices;
+        p2_loss_choices = p2_losses.iter().map(|r| r.choices).sum::<u64>();
+
+        println!(
+            "{} p1 w{}/l{} p2 w{}/l{} p1 wins {} / p2 wins {}",
+            i,
+            p1_win_choices,
+            p1_loss_choices,
+            p2_win_choices,
+            p2_loss_choices,
+            p1_win_sum,
+            p2_win_sum
+        );
+    }
+
+    (p1_win_sum, p2_win_sum)
+}
